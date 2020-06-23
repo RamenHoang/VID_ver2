9d5e6d7f2585ae819715f25edf696e5facbcb472
RamenHoang <anhnguyenhoang321@gmail.com>
2020-06-23 10:31:56 +0700

    v1.0.0 Wifi and HTTP/HTTPS: OK

---
+++ VID_ver2.ino
@@ -0,0 +1,90 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid     = "CTCP Hung Viet Solution";
+const char* password = "hungvietsolution";
+const String serverHttp = "http://192.168.1.80:9080";
+const String serverHttps = "https://vid-service.herokuapp.com";
+
+WiFiClientSecure *client = new WiFiClientSecure;
+
+void setup()
+{
+  Serial.begin(115200);
+  connectWifi();
+}
+
+void loop()
+{
+  Serial.print("connecting to ");
+  Serial.println(serverHttps);
+
+  String payload = httpGet(serverHttps, "/alarm");
+  Serial.println("Payload: ");
+  Serial.println(payload);
+//  JSONVar oPayload = JSON.parse(payload);
+//
+//  Serial.print("name: ");
+//  Serial.println(oPayload["name"]);
+//  Serial.print("year old: ");
+//  Serial.println(oPayload["year_old"]);
+//  Serial.print("school: ");
+//  Serial.println(oPayload["school"]);
+
+  while (1) {
+    reconnectWifi();  
+  }
+}
+
+String httpGet(String server, String api) {
+  HTTPClient http;
+
+  http.begin(*client, server + api);
+
+  // Send HTTP request
+  int httpResponseCode = http.GET();
+
+  String payload = "{}";
+
+  if (httpResponseCode == HTTP_CODE_OK) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void connectWifi() {
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void reconnectWifi() {
+  if (WiFi.status() == WL_DISCONNECTED) {
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+  }
+}
